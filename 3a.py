d8f863d1e4415b39d09f8c73567bc73a519a333e
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-08 14:37:33 +0530

    Create 3a.py

---
+++ 3a.py
@@ -0,0 +1,12 @@
+w11=int(input())
+x1=input().split()
+y1=[]
+for i1 in range(0,w11):
+  if(int(x1[i1])==i1):
+    y1.append(x1[i1])
+if(y1==[]):
+  print("-1")
+else:
+  y.sort()
+  for j1 in range(0,len1(y)):
+    print(y1[j1],end=" ")
